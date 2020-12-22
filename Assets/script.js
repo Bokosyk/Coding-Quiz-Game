19582c7c1c54ef0b4e74cb054ee4e5d0533af0b0
Leandro Parrado <lparrado321@gmail.com>
2020-12-22 17:13:48 -0500

    Removed unnecessary popper and JQuery scripts for now, trying to figure out why start-btn in script.js is not hiding upon clicking.

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,21 @@
-var startBtnEl = document.getElementById("#StartBtn");
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('quest-cont');
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+
+    // Lets console know the quiz has started.
+    console.log('Started')
+    //Hides start button once quiz has begun.
+    startButton.classList.add('hide')
+};
+
+function setNextQuestion() {
+
+};
+
+function selectAnswer() {
+
+
+};
