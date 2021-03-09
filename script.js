561f921d82f384949b04438a8c431190ec825e5d
Leandro Parrado <lparrado321@gmail.com>
2021-03-09 01:33:19 -0500

    Changed styling of boxes

--- script.js
+++ script.js
@@ -1,198 +1,112 @@
-const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
-const questionContainerEl = document.getElementById('quest-cont');
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
-//Shuffles questions
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
 let shuffledQuestions, currentQuestionIndex
 
-//Upon clicking, .addEventListener runs a function.
-startButton.addEventListener('click', startQuiz);
+startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
-function startQuiz() {
-    //Hides start button once quiz has begun.
-    startButton.classList.add('hide');
-
-    //Shuffles questions
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-
-    //Removes 'hide' from question container.
-    questionContainerEl.classList.remove('hide');
-
-    // Runs next question function.
-    setNextQuestion();
-
-};
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
 
 function setNextQuestion() {
-    //Resets everything
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-
-};
-
-function resetState() {
-    clearStatusCLass(document.body)
-    //Hides the next button
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        //only sets if answer is correct
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-
-        button.addEventListener('click', selectAnswer);
-        answerButtonsElement.appendChild(button)
-    })
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
-function selectAnswer() {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-      setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-      nextButton.classList.remove('hide')
-    } else {
-      startButton.innerText = 'Restart'
-      startButton.classList.remove('hide')
-    }
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
 
-};
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    }
-    else {
-        element.classList.add('wrong')
-    }
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
-
-//All of the questions for the quiz go here
 const questions = [
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "Mr.Senor Jalapeno",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "Mr.Senor Jalapeno",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-            { text: '4', correct: true },
-        ]
-    },
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  },
+  {
+    question: 'Who is the best YouTuber?',
+    answers: [
+      { text: 'Web Dev Simplified', correct: true },
+      { text: 'Traversy Media', correct: true },
+      { text: 'Dev Ed', correct: true },
+      { text: 'Fun Fun Function', correct: true }
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
 ]
